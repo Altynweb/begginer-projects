76d285808d3d6ef9353e20d62616eaf3c580cbde
SomethingSomethingLinux <93746147+SomethingSomethingLinux@users.noreply.github.com>
2022-12-21 11:14:11 -0300

    Update app.js

--- Random Quote generator/app.js
+++ Random Quote generator/app.js
@@ -30,6 +30,34 @@ const quotes = [{
     quote: '"It is easier to find men who will to volunteer to die, than to find those who are willing to endure pain with patience."',
     person: 'Julius Caesar'
 },
+{
+    quote: '"Impossible is word to be found only in the dictionary of fools."',
+    person: 'Napoleon Bonaparte'
+},
+{
+    quote: '"Until you spread your wings, you will have no idea how far you can fly."',
+    person: 'Napoleon Bonaparte'
+},
+{
+    quote: '"Success is not final, failure is not fatal: It is the courage to continue that counts."',
+    person: 'Winston S. Churchill'
+},
+{
+    quote: '"We must dare to be great; and we must realize that greatness is the fruit of toil, sacrifice and high courage."',
+    person: 'Theodore Roosevelt'
+},
+{
+    quote: '"You can never cross the ocean until you have the courage to lose sight of the shore."',
+    person: 'Christopher Colombus'
+},
+{
+    quote: '"It is foolish to mourn the men who died. Rather we should thank god that such men lived."',
+    person: 'George Patton'
+},
+{
+    quote: '"Sweat saves blood."',
+    person: 'Erwin Rommel'
+},
 ];
 
 for(let i=0; i<quotes.length; i++){
